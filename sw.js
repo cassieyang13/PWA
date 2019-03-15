75c6872a7c0584077688c032a937b0f902465b33
cassie.yang <cassie.yang@cdskysoft.com>
2019-03-15 09:48:19 +0800

    test

---
+++ sw.js
@@ -0,0 +1,57 @@
+
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+var cacheStorageKey = 'minimal-pwa-1'
+var cacheList=[
+	'/',
+	'index.html',
+	'main.css',
+	'zumeng.png'
+]
+self.addEventListener('install',e =>{
+	e.waitUntil(
+		caches.open(cacheStorageKey)
+			.then((cache) => {
+				cache.addAll(cacheList)
+			})
+			.then(() => {
+				console.log("install")
+				self.skipWaiting()
+			})
+	)
+})
+
+self.addEventListener('fetch',function(e){
+	e.respondWith(
+		caches.match(e.request).then(function(response){
+			if(response != null){
+				return response
+			}
+			return fetch(e.request.url)
+		})
+	)
+})
+self.addEventListener('activate',function(e){
+	e.waitUntil(
+		//获取所有cache名称
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				// 获取所有不同于当前版本名称cache下的内容
+				cacheNames.filter(cacheNames => {
+					return cacheNames !== cacheStorageKey
+				}).map(cacheNames => {
+					return caches.delete(cacheNames)
+				})
+			)
+		}).then(() => {
+			return self.clients.claim()
+		})
+	)
+})
+self.addEventListener('notificationclick', event => {
+	let clickedNotification = event.notification;
+	clickedNotification.close();
+	
+	// 执行某些异步操作，等待它完成
+	let promiseChain = doSomething();
+	event.waitUntil(promiseChain);
+});
